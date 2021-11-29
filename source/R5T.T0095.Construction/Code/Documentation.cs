c228e89985ca2189e888aab1f12d760f078ac453
davidcoats <david@getsafetycone.com>
2021-11-28 23:15:51 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0095.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0095.Construction
+{
+    /// <summary>
+    /// Construction project for OnDisposeAction..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0095/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0095
+{
+    /// <summary>
+    /// OnDisposeAction.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
